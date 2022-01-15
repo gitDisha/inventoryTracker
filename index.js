fb777c1235b5bcdbd1735bb4559b40d16afc6deb
Disha <disha.sn21@gmail.com>
2022-01-16 02:19:42 +0530

    server running

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 const http = require("http");
-const path = require("path");
-const fs = require("fs");
-var bodyParser = require('body-parser');
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyParser = require("body-parser");
 
-
-// const multer = require("multer")
-// const upload = multer({ dest: `./public/uploads` })
-
-// const handleError = (err, res) => {
-//     res
-//         .status(500)
-//         .contentType("text/plain")
-//         .end("Oops! Something went wrong!");
-// };
+// const path = require("path");
+// const fs = require("fs");
 
 const express = require("express");
 const app = express();
 
-app.get("/", function(req,res){
-res.render("list")
-// res.sendFile(__dirname + "/public/index.html");
+dotenv.config({path:'config.env'})
 
+
+//log request
+app.use(morgan('tiny'));
+
+//parsing requests
+app.use(bodyParser.urlencoded({extended: true}))
+
+//sending and receiving requests
+app.get("/", function(req,res){
+    res.send("Hello")
 });
 
 //view engine that helps with templating. Simplest to use
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000;
 
@@ -33,8 +32,8 @@ http.createServer(app).listen(PORT, function() {
     console.log(`Server is running on local port ${PORT}`);
 });
 
-app.post("/", function(req,res){
-    var item = req.body.newitem;
-    console.log(item)
-});
+// app.post("/", function(req,res){
+//     var item = req.body.newitem;
+//     console.log(item)
+// });
 
